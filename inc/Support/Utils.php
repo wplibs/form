b59bc4771cca2c34fe5bc44f5237aad3f3090d27
Nguyen Van Anh <anhskohbo@gmail.com>
2017-06-25 19:22:12 +0700

    Added send download

--- inc/Support/Utils.php
+++ inc/Support/Utils.php
@@ -145,4 +145,26 @@ class Utils {
 
 		return (bool) preg_match( '#^' . $pattern . '\z#u', $value );
 	}
+
+	/**
+	 * Send a download file to client.
+	 *
+	 * @param  string $data     Data to send to client download.
+	 * @param  string $filename Download file name.
+	 */
+	public static function send_download( $data, $filename = null ) {
+		if ( empty( $filename ) ) {
+			$filename = sprintf( '%s.txt', uniqid() );
+		}
+
+		header( 'Content-Type: application/octet-stream; charset=' . get_option( 'blog_charset' ), true );
+		header( 'Content-disposition: attachment; filename=' . $filename );
+		header( 'Content-Transfer-Encoding: binary' );
+		header( 'Pragma: no-cache' );
+		header( 'Expires: 0' );
+
+		// Print data endwith newline.
+		print $data . "\n"; // WPCS: XSS OK.
+		exit;
+	}
 }
