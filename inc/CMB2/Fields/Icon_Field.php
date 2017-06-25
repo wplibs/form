196de5df12e399a75be013c8fb4fd39f133b859e
Nguyen Van Anh <anhskohbo@gmail.com>
2017-06-25 23:33:38 +0700

    Intro iconfonts and icon field.

--- inc/CMB2/Fields/Icon_Field.php
+++ inc/CMB2/Fields/Icon_Field.php
@@ -12,67 +12,78 @@ class Icon_Field extends Field_Abstract {
 	 * @param \CMB2_Types $field_type_object  The `CMB2_Types` object.
 	 */
 	public function output( $field, $escaped_value, $object_id, $object_type, $field_type_object ) {
-		$defaults = array(
-			'id'    => '',
-			'name'  => '',
-			'value' => array(
-				'type' => '',
-				'icon' => '',
-			),
-			'select' => sprintf( '<a class="ipf-select">%s</a>', esc_html__( 'Select Icon', 'icon-picker-field' ) ),
-			'remove' => sprintf( '<a class="ipf-remove button hidden">%s</a>', esc_html__( 'Remove', 'icon-picker-field' ) ),
-		);
+		$escaped_value = wp_parse_args( $escaped_value, array(
+			'type' => '',
+			'icon' => '',
+		));
 
-		$args          = array();
-		$args          = wp_parse_args( $args, $defaults );
-		$args['value'] = wp_parse_args( $args['value'], $defaults['value'] );
+		printf( '<div id="%s" class="ipf">', $field_type_object->_id() );
+		printf( '<a class="ipf-select">%s</a>', esc_html__( 'Select Icon', 'skeleton' ) );
+		printf( '<a class="ipf-remove button hidden">%s</a>', esc_html__( 'Remove', 'skeleton' ) );
 
-		$field  = sprintf( '<div id="%s" class="ipf">', $args['id'] );
-		$field .= $args['select'];
-		$field .= $args['remove'];
+		print $field_type_object->input( array( // WPCS: XSS OK.
+			'type'    => 'hidden',
+			'id'      => $field_type_object->_id( '[type]' ),
+			'name'    => $field_type_object->_name( '[type]' ),
+			'class'   => 'ipf-type',
+			'value'   => $escaped_value['type'],
+		) );
 
-		foreach ( $args['value'] as $key => $value ) {
-			$field .= sprintf(
-				'<input type="hidden" id="%s" name="%s" class="%s" value="%s" />',
-				esc_attr( "{$args['id']}-{$key}" ),
-				esc_attr( "{$args['name']}[{$key}]" ),
-				esc_attr( "ipf-{$key}" ),
-				esc_attr( $value )
-			);
-		}
+		print $field_type_object->input( array( // WPCS: XSS OK.
+			'type'    => 'hidden',
+			'id'      => $field_type_object->_id( '[icon]' ),
+			'name'    => $field_type_object->_name( '[icon]' ),
+			'class'   => 'ipf-icon',
+			'value'   => $escaped_value['icon'],
+		) );
 
 		// This won't be saved. It's here for the preview.
-		$field .= sprintf(
-			'<input type="hidden" class="url" value="%s" />',
-			esc_attr( icon_picker_get_icon_url( $args['value']['type'], $args['value']['icon'] ) )
+		printf( '<input type="hidden" class="url" value="%s">',
+			esc_attr( $this->get_icon_url( $escaped_value['type'], $escaped_value['icon'] ) )
 		);
-		$field .= '</div>';
 
-		echo $field; // xss ok
-	}
-}
+		echo '</div>';
 
-/**
- * Get Icon URL
- *
- * @since  0.2.0
- *
- * @param  string  $type  Icon type.
- * @param  mixed   $id    Icon ID.
- * @param  string  $size  Optional. Icon size, defaults to 'thumbnail'.
- *
- * @return string
- */
-function icon_picker_get_icon_url( $type, $id, $size = 'thumbnail' ) {
-	$url = '';
-
-	if ( ! in_array( $type, array( 'image', 'svg' ) ) ) {
-		return $url;
+		wp_enqueue_media();
+		$field->add_js_dependencies( array( 'icon-picker' ) );
 	}
 
-	if ( empty( $id ) ) {
-		return $url;
+	/**
+	 * Filter the value before it is saved.
+	 *
+	 * @param bool|mixed     $override_value Sanitization/Validation override value to return.
+	 * @param mixed          $value      The value to be saved to this field.
+	 * @param int            $object_id  The ID of the object where the value will be saved.
+	 * @param array          $field_args The current field's arguments.
+	 * @param \CMB2_Sanitize $sanitizer  The `CMB2_Sanitize` object.
+	 */
+	public function sanitization( $override_value, $value, $object_id, $field_args, $sanitizer ) {
+		return wp_parse_args( $value, array(
+			'type' => '',
+			'icon' => '',
+		));
 	}
 
-	return wp_get_attachment_image_url( $id, $size, false );
+	/**
+	 * Get Icon URL
+	 *
+	 * @param  string  $type  Icon type.
+	 * @param  mixed   $id    Icon ID.
+	 * @param  string  $size  Optional. Icon size, defaults to 'thumbnail'.
+	 *
+	 * @return string
+	 */
+	protected function get_icon_url( $type, $id, $size = 'thumbnail' ) {
+		$url = '';
+
+		if ( ! in_array( $type, array( 'image', 'svg' ) ) ) {
+			return $url;
+		}
+
+		if ( empty( $id ) ) {
+			return $url;
+		}
+
+		return wp_get_attachment_image_url( $id, $size, false );
+	}
 }
---
+++ inc/Iconfonts/README.md
@@ -0,0 +1,4 @@
+## Build the JS
+
+git clone https://github.com/kucrut/wp-icon-picker.git
+cd wp-icon-picker
