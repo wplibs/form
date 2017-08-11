76a7044d74542a7e9f451df07f46a17ac915aca9
Nguyen Van Anh <anhskohbo@gmail.com>
2017-08-11 14:07:34 +0700

    Move webfonts.json inside inc

--- skeleton.php
+++ skeleton.php
@@ -12,18 +12,8 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-if ( file_exists( trailingslashit( __DIR__ ) . 'vendor/autoload.php' ) ) {
-	require_once trailingslashit( __DIR__ ) . 'vendor/autoload.php';
-}
-
-// Try locate the CMB2.
-if ( file_exists( trailingslashit( __DIR__ ) . 'vendor/webdevstudios/cmb2/init.php' ) ) {
-	require_once trailingslashit( __DIR__ ) . 'vendor/webdevstudios/cmb2/init.php';
-} elseif ( file_exists( __DIR__ . '/../../webdevstudios/cmb2/init.php' ) ) {
-	require_once __DIR__ . '/../../webdevstudios/cmb2/init.php';
-} else {
-	throw new RuntimeException( 'Unable to locate the CMB2' );
-}
+// First, init the bootstrap.
+require_once trailingslashit( __DIR__ ) . 'bootstrap.php';
 
 // Now boot the Skeleton after WP-init.
 if ( ! defined( 'AWETHEMES_SKELETON_LOADED' ) ) {
